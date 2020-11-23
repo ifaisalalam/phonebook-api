0c96e5770a62410e5f3f661e2d0c47faf31f191a
Faisal Alam <ifaisalalam@users.noreply.github.com>
2020-11-23 17:23:18 +0530

    create Joi validation pipe

---
+++ src/pipes/joi-validation.pipe.spec.ts
@@ -0,0 +1,31 @@
+import { JoiValidationPipe } from './joi-validation.pipe';
+import * as Joi from '@hapi/joi';
+import { BadRequestException } from '@nestjs/common';
+
+interface Value {
+  name: string;
+}
+
+describe('JoiValidationPipe', () => {
+  const joiSchema: Joi.ObjectSchema = Joi.object<Value>({
+    name: Joi.string()
+      .trim()
+      .required(),
+  });
+
+  it('should return validated value', () => {
+    const joiValidationPipe = new JoiValidationPipe(joiSchema);
+    const value: Value = { name: 'John Doe' };
+    const transformedValue = joiValidationPipe.transform(value, {
+      type: 'custom',
+    });
+    expect(transformedValue).toStrictEqual(value);
+  });
+
+  it('should throw exception on validation failure', () => {
+    const joiValidationPipe = new JoiValidationPipe(joiSchema);
+    const value: Partial<Value> = {};
+    const t = () => joiValidationPipe.transform(value, { type: 'custom' })
+    expect(t).toThrowError(BadRequestException);
+  });
+});
